4796b13842c741b044696b599c6996721b46af94
asunluer <116374236+asunluer@users.noreply.github.com>
2024-05-24 16:16:58 +0200

    Add files via upload
    Hayırlı cumalar. 
    Programımız mübarek olsun. 
    Kazanız mübarek olsun

---
+++ sciper1_sciper2_sciper3_project 2/src/methods/deep_network.py
@@ -0,0 +1,274 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+
+## MS2
+
+
+class MLP(nn.Module):
+    """
+    An MLP network which does classification.
+
+    It should not use any convolutional layers.
+    """
+
+    def __init__(self, input_size, n_classes):
+        """
+        Initialize the network.
+        
+        You can add arguments if you want, but WITH a default value, e.g.:
+            __init__(self, input_size, n_classes, my_arg=32)
+        
+        Arguments:
+            input_size (int): size of the input
+            n_classes (int): number of classes to predict
+        """
+        super().__init__()
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+
+    def forward(self, x):
+        """
+        Predict the class of a batch of samples with the model.
+
+        Arguments:
+            x (tensor): input batch of shape (N, D)
+        Returns:
+            preds (tensor): logits of predictions of shape (N, C)
+                Reminder: logits are value pre-softmax.
+        """
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+        return preds
+
+
+class CNN(nn.Module):
+    """
+    A CNN which does classification.
+
+    It should use at least one convolutional layer.
+    """
+
+    def __init__(self, input_channels, n_classes, filters=(16, 32, 64)):
+        """
+        Initialize the network.
+        
+        You can add arguments if you want, but WITH a default value, e.g.:
+            __init__(self, input_channels, n_classes, my_arg=32)
+        
+        Arguments:
+            input_channels (int): number of channels in the input
+            n_classes (int): number of classes to predict (output)
+        """
+        super(CNN, self).__init__()
+        self.conv2d1 = nn.Conv2d(input_channels, filters[0])
+        self.conv2d2 = nn.Conv2d(filter[0], filters[1])
+        self.fc1 = nn.Linear(100, n_classes)
+        
+
+    def forward(self, x):
+        """
+        Predict the class of a batch of samples with the model.
+
+        Arguments:
+            x (tensor): input batch of shape (N, Ch, H, W)
+        Returns:
+            preds (tensor): logits of predictions of shape (N, C)
+                Reminder: logits are value pre-softmax.
+        """
+        x = F.max_pool2d(F.relu(self.conv2d1(x)), 3)
+        x = F.max_pool2d(F.relu(self.conv2d2(x)), 3)
+        x = x.flatten(-3)
+        x = F.relu(self.fc1(x))
+        preds = self.fc2(x)
+        return preds
+
+
+class MyViT(nn.Module):
+    """
+    A Transformer-based neural network
+    """
+
+    def __init__(self, chw, n_patches, n_blocks, hidden_d, n_heads, out_d):
+        """
+        Initialize the network.
+        
+        """
+        super().__init__()
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+
+    def forward(self, x):
+        """
+        Predict the class of a batch of samples with the model.
+
+        Arguments:
+            x (tensor): input batch of shape (N, Ch, H, W)
+        Returns:
+            preds (tensor): logits of predictions of shape (N, C)
+                Reminder: logits are value pre-softmax.
+        """
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+        return preds
+
+
+class Trainer(object):
+    """
+    Trainer class for the deep networks.
+
+    It will also serve as an interface between numpy and pytorch.
+    """
+
+    def __init__(self, model, lr, epochs, batch_size):
+        """
+        Initialize the trainer object for a given model.
+
+        Arguments:
+            model (nn.Module): the model to train
+            lr (float): learning rate for the optimizer
+            epochs (int): number of epochs of training
+            batch_size (int): number of data points in each batch
+        """
+        self.lr = lr
+        self.epochs = epochs
+        self.model = model
+        self.batch_size = batch_size
+
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(model.parameters(), lr)  ### WRITE YOUR CODE HERE
+
+    def train_all(self, dataloader):
+        """
+        Fully train the model over the epochs. 
+        
+        In each epoch, it calls the functions "train_one_epoch". If you want to
+        add something else at each epoch, you can do it here.
+
+        Arguments:
+            dataloader (DataLoader): dataloader for training data
+        """
+        for ep in range(self.epochs):
+            self.train_one_epoch(dataloader, ep)
+
+            ### WRITE YOUR CODE HERE if you want to do add something else at each epoch
+
+    def train_one_epoch(self, dataloader, ep):
+        """
+        Train the model for ONE epoch.
+
+        Should loop over the batches in the dataloader. (Recall the exercise session!)
+        Don't forget to set your model to training mode, i.e., self.model.train()!
+
+        Arguments:
+            dataloader (DataLoader): dataloader for training data
+        """
+        self.model.train()
+
+        for index, batch in enumerate(dataloader):
+            # x contains input features i.e. images 
+            # y contains the corresponding labels i.e. targets  
+            x,y = batch 
+
+            # performs a forward pass through a model
+            logits = self.model(x)
+
+            # computes the loss using the criterion function 
+            loss = self.criterion(logits, y)
+
+            # performs the backpropagation to compute the gradients of the loss w.r.t. model's parameters 
+            torch.autograd.backward(loss)
+
+            # updates the model's parameters which is weights using optimizer 
+            self.optimizer.step()
+
+            #zeros out the gradients in the optimizer to prevent from accumulation from multiple batches 
+            self.optimizer.zero_grad()
+
+            print('\rEp {}/{}, it {}/{}: loss train: {:.2f}, accuracy train: {:.2f}'.
+                  format(ep + 1, self.epochs, index + 1, len(dataloader)), loss, end='')
+
+
+
+    def predict_torch(self, dataloader):
+        """
+        Predict the validation/test dataloader labels using the model.
+
+        Hints:
+            1. Don't forget to set your model to eval mode, i.e., self.model.eval()!
+            2. You can use torch.no_grad() to turn off gradient computation, 
+            which can save memory and speed up computation. Simply write:
+                with torch.no_grad():
+                    # Write your code here.
+
+        Arguments:
+            dataloader (DataLoader): dataloader for validation/test data
+        Returns:
+            pred_labels (torch.tensor): predicted labels of shape (N,),
+                with N the number of data points in the validation/test data.
+        """
+
+        pred_labels = [] 
+        # are batch.size and x.shape[0] same ???? 
+        # puts the model in evaluation mode 
+        self.model.eval()
+        with torch.no_grad(): 
+            for index, batch in enumerate(dataloader): 
+                x, y = batch
+                pred_labels.append(self.model(x))
+        return pred_labels
+    
+    def fit(self, training_data, training_labels):
+        """
+        Trains the model, returns predicted labels for training data.
+
+        This serves as an interface between numpy and pytorch.
+
+        Arguments:
+            training_data (array): training data of shape (N,D)
+            training_labels (array): regression target of shape (N,)
+        Returns:
+            pred_labels (array): target of shape (N,)
+        """
+
+        # First, prepare data for pytorch
+        train_dataset = TensorDataset(torch.from_numpy(training_data).float(), 
+                                      torch.from_numpy(training_labels))
+        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        
+        self.train_all(train_dataloader)
+
+        return self.predict(training_data)
+
+    def predict(self, test_data):
+        """
+        Runs prediction on the test data.
+
+        This serves as an interface between numpy and pytorch.
+        
+        Arguments:
+            test_data (array): test data of shape (N,D)
+        Returns:
+            pred_labels (array): labels of shape (N,)
+        """
+        # First, prepare data for pytorch
+        test_dataset = TensorDataset(torch.from_numpy(test_data).float())
+        test_dataloader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
+
+        pred_labels = self.predict_torch(test_dataloader)
+
+        # We return the labels after transforming them into numpy array.
+        return pred_labels.cpu().numpy()
